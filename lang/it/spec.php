6cd542b537f1f61df2da9963528bf058ef9c99f0
Maicol Battistini <maicolbattistini@live.it>
2022-02-04 18:49:12 +0100

    [Feature] Add Italian translations (#5)

---
+++ lang/it/spec.php
@@ -0,0 +1,166 @@
+<?php
+/*
+ * Copyright 2021 Cloud Creativity Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | JSON:API Specification Errors
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default translatable members
+    | of JSON:API error objects. According to the JSON:API spec, the
+    | `title` and `detail` members can be localized. In addition the `code`
+    | member is also read from this package if you want to give the error
+    | a specific code.
+    |
+    | Set any value to an empty string if you do not want the member to be
+    | included in the error object.
+    |
+    | @see http://jsonapi.org/format/#errors
+    */
+
+    'json_empty' => [
+        'title' => 'JSON non valido',
+        'detail' => 'Previsto un JSON da decodificare.',
+        'code' => '',
+    ],
+
+    'json_error' => [
+        'title' => 'JSON non valido',
+    ],
+
+    'json_not_object' => [
+        'title' => 'JSON non valido',
+        'detail' => 'Previsto un JSON da decodificare in un oggetto.',
+        'code' => '',
+    ],
+
+    'member_required' => [
+        'title' => 'Documento JSON:API non conforme',
+        'detail' => 'Il membro :member è richiesto.',
+        'code' => '',
+    ],
+
+    'member_object_expected' => [
+        'title' => 'Documento JSON:API non conforme',
+        'detail' => 'Il membro :member deve essere un oggetto.',
+        'code' => '',
+    ],
+
+    'member_array_expected' => [
+        'title' => 'Documento JSON:API non conforme',
+        'detail' => 'Il membro :member deve essere un array.',
+        'code' => '',
+    ],
+
+    'member_identifier_expected' => [
+        'title' => 'Documento JSON:API non conforme',
+        'detail' => 'Il membro :member deve essere un identificatore di risorsa.',
+        'code' => '',
+    ],
+
+    'member_string_expected' => [
+        'title' => 'Documento JSON:API non conforme',
+        'detail' => 'Il membro :member deve essere una stringa.',
+        'code' => '',
+    ],
+
+    'member_empty' => [
+        'title' => 'Documento JSON:API non conforme',
+        'detail' => 'Il membro :member non può essere vuoto.',
+        'code' => '',
+    ],
+
+    'member_field_not_allowed' => [
+        'title' => 'Documento JSON:API non conforme',
+        'detail' => 'Il membro :member non può avere un campo :field.',
+        'code' => '',
+    ],
+
+    'member_field_not_supported' => [
+        'title' => 'Documento JSON:API non conforme',
+        'detail' => 'Il campo :field non è un :type supportato.',
+        'code' => '',
+    ],
+
+    'field_expects_to_one' => [
+        'title' => 'Documento JSON:API non conforme',
+        'detail' => 'Il campo :field deve essere una relazione to-one.',
+        'code' => '',
+    ],
+
+    'field_expects_to_many' => [
+        'title' => 'Documento JSON:API non conforme',
+        'detail' => 'Il campo :field deve essere una relazione to-many.',
+        'code' => '',
+    ],
+
+    'resource_type_not_supported' => [
+        'title' => 'Non supportato',
+        'detail' => 'Il tipo di risorsa :type non è supportato da questo endpoint.',
+        'code' => '',
+    ],
+
+    'resource_type_not_supported_by_to_one_relationship' => [
+        'title' => 'Entità non processabile',
+        'detail' => 'Il campo :field deve essere una relazione to-one contenente risorse :types.',
+        'code' => '',
+    ],
+
+    'resource_type_not_supported_by_to_many_relationship' => [
+        'title' => 'Entità non processabile',
+        'detail' => 'Il campo :field deve essere una relazione to-many contenente risorse :types.',
+        'code' => '',
+    ],
+
+    'resource_type_not_recognised' => [
+        'title' => 'Non supportato',
+        'detail' => 'Il tipo di risorsa :type non è riconosciuto.',
+        'code' => '',
+    ],
+
+    'resource_id_not_supported' => [
+        'title' => 'Non supportato',
+        'detail' => 'L\'id risorsa :id non è supportato da questo endpoint.',
+        'code' => '',
+    ],
+
+    'resource_client_ids_not_supported' => [
+        'title' => 'Non supportato',
+        'detail' => 'Il tipo di risorsa :type non supporta gli ID generati dal client.',
+        'code' => '',
+    ],
+
+    'resource_exists' => [
+        'title' => 'Conflitto',
+        'detail' => 'La risorsa :id esiste già.',
+        'code' => '',
+    ],
+
+    'resource_not_found' => [
+        'title' => 'Non trovato',
+        'detail' => 'La risorsa correlata non esiste.',
+        'code' => '',
+    ],
+
+    'resource_field_exists_in_attributes_and_relationships' => [
+        'title' => 'Documento JSON:API non conforme',
+        'detail' => 'Il campo :field non può esistere come attributo e come relazione.',
+        'code' => '',
+    ],
+];
